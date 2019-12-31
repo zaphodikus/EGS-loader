034f33a1123268ffe897a10005a0a8962c2411f4
zaphodikus <zaphodikus@hotmail.com>
2019-12-31 17:57:49 +0000

    handle comment lines, add exception FW

--- egs_read.py
+++ egs_read.py
@@ -4,6 +4,7 @@
 # "C:\ssd\Steam\SteamApps\common\Empyrion - Galactic Survival\Content\Configuration\TraderNPCConfig.ecf"
 # https://empyrion.gamepedia.com/Game_API_tuts_configecf
 import re
+from enum import Enum
 
 def find_ecf_value_MoneyCard(output):
     # expr = \{\s*(?:(?!Name).)*Name:[ ]*MoneyCard(?:(?!StackSize:|\})(?:.|\n))*StackSize:[ ]*([0-9]*)
@@ -12,7 +13,37 @@ def find_ecf_value_MoneyCard(output):
         print(x.group(1))
 
 
-def load_ecf(config_file = "C:\\ssd\\Steam\\SteamApps\\common\\Empyrion - Galactic Survival\\Content\\Configuration\\Config_Example.ecf", debug = False):
+# class to define the kinds of ecf file we cater for
+class ECFType(Enum):
+    AutoDetect=0
+    Config=1    # Config_example/Config
+    Trader=2    # TraderNPCConfig
+    Faction=3   # FactionWarfare
+    EGroups=4   # EGroupsConfig
+    Reputation=5  # DefReputation
+    Shapes=6    # BlockShapesWindow
+
+class ECFException(Exception):
+    """base class to define the parser exceptions we support"""
+    pass
+
+class ECFCollision(ECFException):
+    def __index__(self, a, b, msg="ECF key collision"):
+        super().__init__(msg)
+        _context = a + b
+
+
+class ECFExpectedInteger(ECFException):
+    def __index__(self, context=None, msg="ECF expected an integer"):
+        super().__init__(msg)
+        _context = context
+
+class ECFExpectedString(ECFException):
+    def __index__(self, context=None, msg= "ECF expected a string"):
+        super().__init__(msg)
+        _context = context
+
+def load_ecf(config_file = "C:\\ssd\\Steam\\SteamApps\\common\\Empyrion - Galactic Survival\\Content\\Configuration\\Config_Example.ecf", type=ECFType.AutoDetect, debug = False):
     output = open(config_file,'r').read()
     ecfimport = {}      #Allow for String => int lookup
     ecfimportOrig = {}  #Allow for int => string lookup for blocks only
@@ -21,7 +52,15 @@ def load_ecf(config_file = "C:\\ssd\\Steam\\SteamApps\\common\\Empyrion - Galact
     ecfImportScenario = {}  # Faction scenario
     ecfImportUnit = {}      # Faction units
 
-    st = output.split('{')
+    version = re.search("VERSION:\\s([\\d]*)", output) # only in the Config.ecf file
+    if version:
+        version = version.group(1)
+    outputlines = output.split('\n')
+    outputclean=""
+    for line in outputlines:
+        if len(line) and line[0] != '#':
+            outputclean+=line + '\n'
+    st = outputclean.split('{')
     for ss in st:
         #print(ss)
         te = -1
@@ -32,39 +71,67 @@ def load_ecf(config_file = "C:\\ssd\\Steam\\SteamApps\\common\\Empyrion - Galact
         faction_unit = ""
         sb = re.split(",|\n",ss)
         for se in sb:
-            #print(se)
+            # print(se)
+
             if te == -1 and se.__contains__("Id:"):
                 #print("[ID] {0}".format(se))
                 try:
                     te = int(se.split(':')[1].strip())
+                    if se.__contains__("Block ") and type==ECFType.AutoDetect:  # autodetect logic
+                        type = ECFType.Config
                 except Exception as e: # TODO: gobble handle numberic parse errors here
                     print("Exception:{0}".format(e))
+                    raise ECFExpectedInteger(str(e))
             elif ts == "" and se.__contains__("Name:"):
                 #print("[NAME] {0}".format(se))
                 try:
                     ts = se.split(':')[1].strip()
                     if se.__contains__("Trader "):  # Traders don't have block id's
+                        if type == ECFType.AutoDetect:  # autodetect logic
+                            type = ECFType.Trader
                         te = 1
                         trader_ID = ts
+                    elif se.__contains__(" FactionSettings"):  # Faction name
+                        if type == ECFType.AutoDetect:  # autodetect logic
+                            type = ECFType.Faction
+                        te = 1
+                        faction_name = re.search(".*Faction:\\s([a-zA-Z]*)", ss).group(1)
+                    elif se.__contains__(" Unit"):  # unit class
+                        te = 1
+                        faction_unit = re.search(".*ClassName:\\s([a-zA-Z]*)", ss).group(1)
+                    elif se.__contains__(" Scenario"):  # unit class
+                        te = 1
+                        m = re.findall(".*\\sName:\\s([a-zA-Z0-9\\d]*)", ss)
+                        faction_scenario = m[1] # second match in a findall
+
                 except Exception as e: # TODO: gobble handle text/alphabetics type errors here
                     print("Exception:{0}".format(e))
+                    raise ECFExpectedString(str(e))
             if te != -1 and ts != "":
                 break
         if te != -1 and ts != "":
             if ts.lower().strip() in ecfimport.keys():
                 if debug:
                     print("key collision/duplicate: {0} with {1}".format(ts,te))
+                    raise ECFCollision(ts,te)
             else:
                 if trader_ID != "":
                     ecfImportTrader[trader_ID] = sb  # Traders have no ID, just dump their data in here for now
+                elif faction_name != "":
+                    ecfImportFaction[faction_name] = sb  # faction name
+                elif faction_scenario != "":
+                    ecfImportScenario[faction_scenario] = sb  # faction scenario
+                elif faction_unit != "":
+                    ecfImportUnit[faction_unit] = sb # faction unit
                 else:
                     ecfimport[ts.lower().strip()] = te
                     ecfimportOrig[te] = ts.lower().strip()
     if debug:
         print("ECF loaded: {0}".format(config_file))
-    if len(ecfImportTrader.keys()):
+
+    if ECFType.Trader == type:
         return  ecfImportTrader
-    elif len(ecfImportFaction.keys()):
+    elif ECFType.Faction == type:
         return (ecfImportFaction,ecfImportScenario,ecfImportUnit)
     else:
         return (ecfimport, ecfimportOrig)
--- test_load.py
+++ test_load.py
@@ -1,5 +1,7 @@
 import pytest
 from egs_read import load_ecf
+from egs_read import ECFType
+
 
 # 1588 objects expected
 def test_numobjects_loaded_ExampleECF():
@@ -7,11 +9,13 @@ def test_numobjects_loaded_ExampleECF():
     assert len(v[0].keys()) == 1588
     assert len(v[1].keys()) == 1588
 
+
 # 16 objects expected
 def test_numobjects_loaded_ConfigECF():
     v= load_ecf("TESTDATA\\Config.ecf")
     assert len(v[0].keys()) == 16
     assert len(v[1].keys()) == 16
+
 
 # 37 Traders expected
 def test_numobjects_loaded_NPCTraderECF():
@@ -19,25 +23,37 @@ def test_numobjects_loaded_NPCTraderECF():
     assert len(v.keys()) == 37
     # Traders don't have ID's
 
+
+# 0 Traders expected
+def test_numobjects_loaded_NPCTraderECF_negative_commented():
+    v= load_ecf("NEGDATA\\TraderNPCConfig_commentedout.ecf", type= ECFType.Trader) # force Trader
+    assert len(v.keys()) == 0
+    # Traders don't have ID's
+
+
 # 37 Units/Factions/Scenarios
 def test_numobjects_loaded_FactionECF():
     v= load_ecf("TESTDATA\\FactionWarfare.ecf")
-    assert len(v[0].keys()) == 37
-    assert len(v[1].keys()) == 37
+    assert len(v[0].keys()) == 2  # factions
+    assert len(v[1].keys()) == 27  # scenarios - includes a comment-block!
+    assert len(v[2].keys()) == 8  # units
 
-# 37 Traders expected
+
+# 37 EGroups expected
 def test_numobjects_loaded_GroupsECF():
     v= load_ecf("TESTDATA\\EGroupsConfig.ecf")
     assert len(v[0].keys()) == 37
     assert len(v[1].keys()) == 37
 
-# 37 Traders expected
+
+# 7 Shapes expected
 def test_numobjects_loaded_BlockShapesECF():
     v= load_ecf("TESTDATA\\BlockShapesWindow.ecf")
     assert len(v[0].keys()) == 7
     assert len(v[1].keys()) == 7
 
-# 37 Traders expected
+
+# 37 Reputations expected
 def test_numobjects_loaded_ReputationECF():
     v= load_ecf("TESTDATA\\DefReputation.ecf")
     assert len(v[0].keys()) == 37
